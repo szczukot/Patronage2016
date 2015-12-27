e221ebf62f51500fda4c4ca9f1c770a243609b77
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2015-12-27 23:33:52 +0100

    Initial Commit

---
+++ Patronage2016/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Patronage2016.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string MSG { get; set; }
+        public MainViewModel()
+        {
+            MSG = "Test Message";
+        }
+    }
+}
